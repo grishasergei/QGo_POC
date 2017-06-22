ebf92353185f0412708f742610b79eabb679f663
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2017-06-23 00:22:40 +0200

    Renamed run script to prepare

--- prepare.py
+++ prepare.py
@@ -1,15 +1,16 @@
 from prepare.prepare import prepare_training_data
 from utils.explorer import empty_dir, create_dir
+import numpy as np
 
 
-img_dir = './data/raw/images_test/'
-markers_dir = './data/raw/markers_test/'
+img_dir = './data/raw/images/'
+markers_dir = './data/raw/markers/'
 
-patches_dir = './data/train/img/'
+patches_dir = './data/train/img/img'
 create_dir(patches_dir)
 empty_dir(patches_dir)
 
-density_maps_dir = './data/train/target/'
+density_maps_dir = './data/train/target/target'
 create_dir(density_maps_dir)
 empty_dir(density_maps_dir)
 
@@ -17,6 +18,5 @@ prepare_training_data(img_path=img_dir,
                       markers_path=markers_dir,
                       patch_size=(128, 128),
                       patch_overlay=0,
-                      #[0.5, 0.75, 1, 1.5],
-                      scales=[1],
+                      scales=np.arange(0.5, 1.3, 0.1),
                       out_path=[patches_dir, density_maps_dir])
