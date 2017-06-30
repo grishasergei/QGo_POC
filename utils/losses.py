2fbfb3e01db4f4d25acead31237f05e4c438e9af
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2017-06-30 22:54:55 +0200

    Added euclidean distance loss function

---
+++ utils/losses.py
@@ -0,0 +1,12 @@
+import keras.backend as K
+
+
+def euclidean_distance_loss(y_true, y_pred):
+    """
+    Euclidean distance loss
+    https://en.wikipedia.org/wiki/Euclidean_distance
+    :param y_true: tensor
+    :param y_pred: tensor
+    :return: float
+    """
+    return K.sqrt(K.sum(K.square(y_pred - y_true), axis=-1))
