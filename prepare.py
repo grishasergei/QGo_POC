e3f55b670cab7a35da464ed8898d285a2355b8b7
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2017-08-03 20:50:06 +0200

    Made paths os independent in prepare script

--- prepare.py
+++ prepare.py
@@ -1,16 +1,17 @@
 from prepare.prepare import prepare_training_data
 from utils.explorer import empty_dir, create_dir
 import numpy as np
+from os.path import join
 
 
-img_dir = './data/raw/images/'
-markers_dir = './data/raw/markers/'
+img_dir = join('data', 'raw', 'images')
+markers_dir = join('data', 'raw', 'markers')
 
-patches_dir = './data/train/img/img'
+patches_dir = join('data', 'train', 'img', 'img')
 create_dir(patches_dir)
 empty_dir(patches_dir)
 
-density_maps_dir = './data/train/target/target'
+density_maps_dir = join('data', 'train', 'target', 'target')
 create_dir(density_maps_dir)
 empty_dir(density_maps_dir)
 
