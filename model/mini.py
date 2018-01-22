a54b02fbcb46d975feba3753c7ab6f4dc351ee79
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2018-01-22 20:37:26 +0100

    Modified mini model

--- model/mini.py
+++ model/mini.py
@@ -1,37 +1,35 @@
 from .base import _ModelBase
-from keras.layers import Conv2D, MaxPooling2D, Input, AvgPool2D, Merge, UpSampling2D
+from keras.layers import Conv2D, MaxPooling2D, Input, Flatten, Dense, Dropout
 from keras.models import Model
-from keras.regularizers import l2
 
 
 class QgoMini(_ModelBase):
 
-    def __init__(self):
-        self.name = 'qgo_mini'
+    def __init__(self, *args, **kwargs):
+        super(QgoMini, self).__init__(*args, **kwargs)
+        self.name = 'qgomini'
 
     def _model(self, input_shape):
-        """
-
-        :param input_shape: (int, int, int)
-        :return: Keras sequential model
-        """
         input_layer = Input(input_shape)
 
-        regularizer_0 = l2(0.0001)
+        x = Conv2D(8, (5, 5), activation='relu')(input_layer)
+        x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
 
-        x = Conv2D(24, (5, 5), activation='relu', padding='same', activity_regularizer=regularizer_0)(input_layer)
-        x = AvgPool2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
+        x = Conv2D(16, (5, 5), activation='relu')(x)
+        x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
 
-        x = Conv2D(24, (5, 5), activation='relu', padding='same', activity_regularizer=regularizer_0)(x)
-        x = AvgPool2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
+        x = Conv2D(32, (5, 5), activation='relu')(x)
+        x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
 
-        x = Conv2D(24, (5, 5), activation='relu', padding='same', activity_regularizer=regularizer_0)(x)
-        x = AvgPool2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
+        x = Flatten()(x)
+        x = Dropout(0.5)(x)
 
-        x = Conv2D(1, (1, 1), padding='same', activation='relu', activity_regularizer=regularizer_0)(x)
-        x = UpSampling2D(size=(8, 8))(x)
+        x = Dense(128, activation='relu')(x)
+        x = Dropout(0.5)(x)
 
-        model = Model(input_layer, x, name='qgo_mini')
+        x = Dense(1)(x)
+
+        model = Model(input_layer, x, name=self.name)
 
         return model
 
